fdef7d000c604ad258ba8873b8102a595619a18f
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-11-21 17:44:29 +0300

    UserDetailsService created

---
+++ examserver/src/main/java/com/examenv/examserver/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.examenv.examserver.service.impl;
+
+import com.examenv.examserver.model.User;
+import com.examenv.examserver.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        if (user == null){
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("No user found!!");
+        }
+        return user;
+    }
+}
