5727057d6df152c9407958ea4ee5d15b4b56ee17
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-11-26 16:42:37 +0300

    Created logout and username section for each pages

--- examenvfront/src/app/components/navbar/navbar.component.ts
+++ examenvfront/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public login: LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  public logout(){
+    this.login.logout();
+    window.location.reload();
   }
 
 }
--- examenvfront/src/app/pages/login/login.component.ts
+++ examenvfront/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
 
 @Component({
@@ -14,7 +15,7 @@ export class LoginComponent implements OnInit {
     password: '',
   };
 
-  constructor(private snack:MatSnackBar, private login: LoginService) { }
+  constructor(private snack:MatSnackBar, private login: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -49,10 +50,12 @@ export class LoginComponent implements OnInit {
             // redirect... admin: admin-dashboard
             // redirect... normal: normal-dashboard
             if (this.login.getUserRole() == "ADMIN"){
-              window.location.href = '/admin';
+              //window.location.href = '/admin';
+              this.router.navigate(['admin']);
 
             }else if (this.login.getUserRole() == "NORMAL"){
-              window.location.href = '/user-dashboard';
+             //window.location.href = '/user-dashboard';
+             this.router.navigate(['user-dashboard']);
             }else {
               this.login.logout();
             }
