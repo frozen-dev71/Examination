4ca6c228f72fca90fa5021cce02dba9454a8f0f9
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-11-26 16:31:17 +0300

    Created a guard for normalDashboard page

--- examenvfront/src/app/app-routing.module.ts
+++ examenvfront/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
 import { AdminGuard } from './services/admin.guard';
+import { NormalGuard } from './services/normal.guard';
 
 const routes: Routes = [
   {
@@ -32,7 +33,8 @@ const routes: Routes = [
   {
     path:'user-dashboard',
     component: UserDashboardComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
+    canActivate: [NormalGuard],
   },
 
 
--- examenvfront/src/app/services/normal.guard.ts
+++ examenvfront/src/app/services/normal.guard.ts
@@ -6,20 +6,19 @@ import { LoginService } from './login.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AdminGuard implements CanActivate {
+export class NormalGuard implements CanActivate {
   constructor(private login: LoginService,private router: Router){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      
-      if (this.login.isLoggedIn() && this.login.getUserRole() == 'ADMIN'){
+
+      if (this.login.isLoggedIn() && this.login.getUserRole() == 'NORMAL'){
         return true;
       }
 
       this.router.navigate(['login']);
 
-   
-      return false;
+    return false;
   }
   
 }
