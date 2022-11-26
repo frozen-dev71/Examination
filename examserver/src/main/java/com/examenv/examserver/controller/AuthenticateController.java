e89083eaa9811c98816aa35a2b2c0a23c9e4e536
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-11-26 15:46:49 +0300

    Created getCurrentUser details func

--- examserver/src/main/java/com/examenv/examserver/controller/AuthenticateController.java
+++ examserver/src/main/java/com/examenv/examserver/controller/AuthenticateController.java
@@ -3,6 +3,7 @@ package com.examenv.examserver.controller;
 import com.examenv.examserver.config.JwtUtils;
 import com.examenv.examserver.model.JwtRequest;
 import com.examenv.examserver.model.JwtResponse;
+import com.examenv.examserver.model.User;
 import com.examenv.examserver.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +13,9 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @CrossOrigin("*")
@@ -64,6 +64,12 @@ public class AuthenticateController {
 
             throw new Exception("Incalid Credentials" + e.getMessage());
         }
+    }
+
+    //Rturn details of current user
+    @GetMapping("/current-user")
+    public User getCurrentUser(Principal principal){
+        return ((User)this.userDetailsService.loadUserByUsername(principal.getName()));
     }
 
 }
