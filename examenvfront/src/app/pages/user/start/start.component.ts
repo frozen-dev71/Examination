b8b17d0cb7bcda92f46a5075fa4eea4deffd81df
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-11-30 16:49:08 +0300

    Evaluated the correct answers and displayed the result

--- examenvfront/src/app/pages/user/start/start.component.ts
+++ examenvfront/src/app/pages/user/start/start.component.ts
@@ -14,6 +14,12 @@ export class StartComponent implements OnInit {
   qId:any;
   questions:any;
 
+  marksGained=0;
+  correctAnswers = 0;
+  attempted = 0;
+
+  isSubmit = false;
+
   constructor(private locationSt: LocationStrategy, private _router: ActivatedRoute, private _question: QuestionService) { }
 
   ngOnInit(): void {
@@ -26,6 +32,12 @@ export class StartComponent implements OnInit {
    this._question.getQuestionsOfQuizForTest(this.qId).subscribe(
     (data:any)=>{
      this.questions = data;
+     this.questions.forEach(
+      (q:any)=>{
+          q['givenAnswer'] = '';
+      },
+     );
+     console.log(this.questions);
     },
     (error)=>{
       Swal.fire("Error","Error in loading questions of quiz","error");
@@ -38,6 +50,36 @@ export class StartComponent implements OnInit {
     this.locationSt.onPopState(
       ()=>{
         history.pushState(null,'',location.href);
+      }
+    );
+  }
+
+  submitQuiz(){
+    Swal.fire(
+      {
+        title: 'Do you want to submit the quiz?',
+        showCancelButton: true,
+        confirmButtonText: 'Submit',
+        icon: 'info'
+      }
+    ).then(
+      (result)=>{
+        if(result.isConfirmed){
+          this.isSubmit = true;
+          this.questions.forEach(
+            (q:any)=>{
+
+              if(q.givenAnswer == q.answer){
+                this.correctAnswers++;
+               let markSingle = this.questions[0].quiz.maxMark / this.questions.length;
+               this.marksGained += markSingle;
+              }
+              if(q.givenAnswer.trim()!=''){
+                this.attempted++;
+              }
+            }
+          );
+        }
       }
     );
   }
